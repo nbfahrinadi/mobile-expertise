5a24bf625148ff88236371758fece2ee22ea4a50
Nabil <nbfahrinadi@gmail.com>
2025-07-21 09:14:02 +0700

    fixing splash logo

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -3,4 +3,5 @@ class AppVectors {
   static const String format = '.svg';
 
   static const String logo = '${basePath}adamlabs_logo$format';
+  // static const String logo = '${basePath}logo_nb$format';
 }
